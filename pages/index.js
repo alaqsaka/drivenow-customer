ce83bd42bb5c51c7b5c4d0bad364ac9fa816c82a
alaqsaka <aqsaabidin15@gmail.com>
2022-12-01 22:21:22 +0700

    chore: fix importing contactus on indexjs

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Hero from "./components/Hero";
 import FeaturesAsymmetrical from "./components/LandingPage/Features";
 import Navbar from "./components/Navbar";
 import Cars from "./components/parts/Cars";
-import { ContactUs } from "./components/parts/ContactUs";
+import ContactUs from "./components/parts/ContactUs";
 
 export default function IndexPage() {
   return (
